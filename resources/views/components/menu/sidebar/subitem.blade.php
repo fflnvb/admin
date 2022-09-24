1d26df56d315db128b13caa0dca78eb784d1d753
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-09-24 05:43:28 +0200

    Align text subitem, change color

--- resources/views/components/menu/sidebar/subitem.blade.php
+++ resources/views/components/menu/sidebar/subitem.blade.php
@@ -1,7 +1,6 @@
 <li class="nav-item ms-2">
     <a href="{{ route($subitem['route']) }}"
-        class="nav-link
-        {{ $active ? 'bg-light bg-opacity-10' : '' }}">
+        class="nav-link text-white d-flex align-items-center {{ $active ? 'bg-light bg-opacity-10' : '' }}">
         <i class="bi-{{ $subitem['icon'] }} me-2" style="font-size: 1.2rem"></i>
         {{ $subitem['name'] }}
     </a>
