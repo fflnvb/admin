7312ee42083145f063c45babeb714486d177e565
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-09-24 06:14:57 +0200

    Add model id in sidebar

--- resources/views/components/mask/info.blade.php
+++ resources/views/components/mask/info.blade.php
@@ -2,6 +2,10 @@
 <div class="col-12 col-lg-4 col-xl-3">
     <div class="list-group shadow">
         <x-admin::mask.item>
+            <h5>Id</h5>
+            {{ $model->id }}
+        </x-admin::mask.item>
+        <x-admin::mask.item>
             <h5>{{ __('admin::directives.createdAt') }}</h5>
             {{ $model->prettyDate('created_at') ?? '-' }}
         </x-admin::mask.item>
