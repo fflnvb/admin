e83e0104fd5873868303ac91352d1973ed02d9b6
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-09-24 05:53:10 +0200

    Fix broken form, invalid because of nesting

--- resources/views/components/mask/edit.blade.php
+++ resources/views/components/mask/edit.blade.php
@@ -1,22 +1,21 @@
-<form method="POST"
-    action="{{ isset($model->id) ? route('admin.' . $routeName . '.update', $model->id) : route('admin.' . $routeName . '.store') }}">
 
+{{-- Headline --}}
+<div class="d-flex flex-column flex-md-row align-items-start align-items-md-center mb-4">
+    <h1 class="m-0 flex-grow-1 mb-1 mb-md-0">{{ isset($model->id) ? __('admin::directives.editWithName', ['name' => $name]) : __('admin::directives.createWithName', ['name' => $name]) }}</h1>
+    @isset($model->id)
+    <form action="{{ route('admin.' . $routeName . '.destroy', $model->id) }}" method="POST">
+        @method('DELETE')
+        @csrf
+        <button type="submit" onclick="return confirm('{{ __('admin::directives.areYouSure') }}')"
+            class="btn btn-danger"><b>{{ __('admin::directives.delete') }}</b>
+        </button>
+    </form>
+    @endisset
+</div>
+{{-- Cards --}}
+<form method="POST" action="{{ isset($model->id) ? route('admin.' . $routeName . '.update', $model->id) : route('admin.' . $routeName . '.store') }}">
     @csrf
     @isset($model->id) @method('PUT') @endisset
-    {{-- Headline --}}
-    <div class="d-flex flex-column flex-md-row align-items-start align-items-md-center mb-4">
-        <h1 class="m-0 flex-grow-1 mb-1 mb-md-0">{{ isset($model->id) ? __('admin::directives.editWithName', ['name' => $name]) : __('admin::directives.createWithName', ['name' => $name]) }}</h1>
-        @isset($model->id)
-        <form action="{{ route('admin.' . $routeName . '.destroy', $model->id) }}" method="POST">
-            @method('DELETE')
-            @csrf
-            <button type="submit" onclick="return confirm('{{ __('admin::directives.areYouSure') }}')"
-                class="btn btn-danger"><b>{{ __('admin::directives.delete') }}</b>
-            </button>
-        </form>
-        @endisset
-    </div>
-    {{-- Cards --}}
     <div class="row gy-3">
         <div class="col">
             <div class="list-group shadow">
@@ -33,5 +32,4 @@
         <button type="submit" class="btn btn-success me-1"><i class="bi bi-check-lg"></i> <b>{{ __('admin::directives.save') }}</b></button>
         <a href="{{ route('admin.' . $routeName . '.index') }}" role="button" class="btn btn-outline-secondary">{{ __('admin::directives.back') }}</a>
     </div>
-
 </form>
