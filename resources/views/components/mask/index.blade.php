5bbf956775894866cc57d78ede4c4bc26627f98d
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-10-15 00:43:44 +0200

    Fix avoid error on empty prelist slot

--- resources/views/components/mask/index.blade.php
+++ resources/views/components/mask/index.blade.php
@@ -4,7 +4,9 @@
     <a href="{{ route('admin.' . $routeName . '.create') }}" role="button" class="btn btn-success">{{ __('admin::directives.createWithName', ['name' => $single]) }}</a>
     @endif
 </div>
-{{ $prelist }}
+@if ($prelist->isNotEmpty())
+    {{ $prelist }}
+@endif
 @if (session('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         {{ session('success') }}
