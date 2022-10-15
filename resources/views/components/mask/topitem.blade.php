5d14b42cb3955c019ea994f4c123a6cf89e8e81f
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-10-15 20:32:14 +0200

    Add Topitem Mask Component

---
+++ resources/views/components/mask/topitem.blade.php
@@ -0,0 +1,15 @@
+<div class="col-12 col-xl-6 col-xxl-4 d-flex align-items-stretch">
+    <div class="card mb-3 shadow flex-grow-1">
+        <div class="row g-0">
+            <div class="col-sm-4 d-flex justify-content-center align-items-center">
+                <h1 class="display-4 m-0">{{ $number }}</h1>
+            </div>
+            <div class="col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $name }}</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
