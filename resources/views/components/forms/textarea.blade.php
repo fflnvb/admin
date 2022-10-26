707f200be0b9b8cd4e25e2a7b68cb13754043eb3
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-10-26 22:16:02 +0200

    Improve textarea height

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -1,6 +1,6 @@
 <div class="input-group mb-3">
     <div class="form-floating">
-        <textarea class="form-control @error($name) is-invalid @enderror" id="{{ $id }}" name="{{ $name }}" placeholder="{{ $placeholder }}" {{ $mandatory ? 'required' : ''}} style="height:120px">{{ old($name) ?: $value }}</textarea>
+        <textarea class="form-control @error($name) is-invalid @enderror" id="{{ $id }}" name="{{ $name }}" placeholder="{{ $placeholder }}" {{ $mandatory ? 'required' : ''}} style="height:200px">{{ old($name) ?: $value }}</textarea>
         <label for="{{ $id }}">{{ $label }}{{ $mandatory ? '*' : ''}}</label>
     </div>
 </div>
