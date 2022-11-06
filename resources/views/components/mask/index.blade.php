21fdb45f2238360dc38ddb2c94c4420e04ce3377
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-11-07 00:42:29 +0100

    Fix index mask create route

--- resources/views/components/mask/index.blade.php
+++ resources/views/components/mask/index.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex flex-column flex-md-row align-items-start align-items-md-center mb-4">
     <h1 class="m-0 flex-grow-1 mb-1 mb-md-0">{{ $name }}</h1>
-    <a href="{{ route('admin' . $routeName . 'create', $createParameter) }}" role="button" class="btn btn-success">{{ __('admin::directives.createWithName', ['name' => $single]) }}</a>
+    <a href="{{ route('admin.' . $routeName . '.create', $createParameter) }}" role="button" class="btn btn-success">{{ __('admin::directives.createWithName', ['name' => $single]) }}</a>
 </div>
 @if (isset($prelist))
     {{ $prelist }}
