70b100719a7571c859e1b8f677f7a7d25ca05ec7
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-11-10 00:23:14 +0100

    Fix optional create button on index

--- resources/views/components/mask/index.blade.php
+++ resources/views/components/mask/index.blade.php
@@ -1,6 +1,8 @@
 <div class="d-flex flex-column flex-md-row align-items-start align-items-md-center mb-4">
     <h1 class="m-0 flex-grow-1 mb-1 mb-md-0">{{ $name }}</h1>
+    @if(Route::has('admin.' . $routeName . '.create', $createParameter))
     <a href="{{ route('admin.' . $routeName . '.create', $createParameter) }}" role="button" class="btn btn-success">{{ __('admin::directives.createWithName', ['name' => $single]) }}</a>
+    @endif
 </div>
 @if (isset($prelist))
     {{ $prelist }}
