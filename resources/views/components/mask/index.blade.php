9741c60e6f427d73dc66335ab4aabb7f03243df2
fflnvb <10604800+fflnvb@users.noreply.github.com>
2022-11-21 21:02:49 +0100

    Fix create button on index

--- resources/views/components/mask/index.blade.php
+++ resources/views/components/mask/index.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex flex-column flex-md-row align-items-start align-items-md-center mb-4">
     <h1 class="m-0 flex-grow-1 mb-1 mb-md-0">{{ $name }}</h1>
-    @if(Route::has('admin.' . $routeName . '.create', $createParameter))
-    <a href="{{ route('admin.' . $routeName . '.create', $createParameter) }}" role="button" class="btn btn-success">{{ __('admin::directives.createWithName', ['name' => $single]) }}</a>
+    @if(Route::has('admin.' . $routeName . '.create'))
+    <a href="{{ route('admin.' . $routeName . '.create') }}" role="button" class="btn btn-success">{{ __('admin::directives.createWithName', ['name' => $single]) }}</a>
     @endif
 </div>
 @if (isset($prelist))
